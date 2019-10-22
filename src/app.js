c7db123b2d715757f6ef0b3afbd981617e147c31
Stephen Boyle <1nobad@gmail.com>
2019-10-22 15:30:23 +0100

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      countries: null,
+    },
+
+    mounted() {
+      this.fetchCountry();
+    },
+
+    filteredCountries: function(name) {
+      return this.countries.filter(country => this.country.name)
+    },
+
+    methods: {
+      fetchCountry: function() {
+        fetch("https://restcountries.eu/rest/v2/all")
+        .then(response => response.json())
+        // .then(data => console.log(data))
+        .then(data => this.countries = data)
+      }
+    }
+  })
+})
